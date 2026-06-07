c5170bfa6e1f174c4cfbadf2776d1bc2cda43864
Yvis <Yvisdev01@gmail.com>
2026-06-07 20:41:29 -0300

    primeiro commit

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+function togglemode() {
+  const html = document.documentElement
+
+  //    if (html.classList.contains("branco")) {
+  //  html.classList.remove("branco")
+  //   }  else {
+  //  html.classList.add("branco")
+  html.classList.toggle("branco")
+}
